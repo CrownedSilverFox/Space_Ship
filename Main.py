6fd295448b6eb57f0617a30487745149a898aee7
crownedsilverfox <byskillcfpro@gmail.com>
2015-12-11 20:28:05 +0700

    Complete to Teleporting.

---
+++ Main.py
@@ -0,0 +1,97 @@
+import sys
+import pygame
+from Vectors import Vector
+
+NORMAL = 0
+TURN_LEFT = 1
+TURN_RIGHT = 2
+SLOWLY = 3
+FASTER = 4
+FPS = 40
+SIZE = (800, 600)
+
+
+class Ship:
+    def __init__(self, pos):
+        self.pos = Vector(pos)
+        self.speed = Vector((1, 0))
+        self.image = pygame.Surface((100, 100))
+        self.status = NORMAL
+        self.vect = self.speed
+        self.speed_up = 0.4
+
+    def move(self):
+        if self.status == TURN_LEFT:
+            self.speed.rotate(-10)
+        elif self.status == TURN_RIGHT:
+            self.speed.rotate(10)
+        elif self.status == SLOWLY:
+            if self.speed.get_len() == 0:
+                return
+            if self.speed.get_len() < 1:
+                self.vect = self.speed.normal() * self.speed_up
+                self.speed = Vector((0, 0))
+            else:
+                self.speed -= (self.speed.normal())
+        elif self.status == FASTER:
+            if self.speed.get_len() == 0:
+                self.speed = self.vect
+            self.speed += self.speed.normal() * self.speed_up
+        self.pos += self.speed
+        if self.pos.x > SIZE[0]:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = SIZE[0]
+        if self.pos.y > SIZE[1]:
+            self.pos.y = 0
+        if self.pos.y < 0:
+            self.pos.y = SIZE[1]
+
+    def events(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.status = TURN_LEFT
+            elif event.key == pygame.K_RIGHT:
+                self.status = TURN_RIGHT
+            elif event.key == pygame.K_DOWN:
+                self.status = SLOWLY
+            elif event.key == pygame.K_UP:
+                self.status = FASTER
+        elif event.type == pygame.KEYUP:
+            self.status = NORMAL
+
+    def update(self):
+        self.move()
+
+    def render(self, screen):
+        screen.blit(self.image, self.pos)
+
+    def draw(self):
+        pygame.draw.rect(self.image, (100, 100, 255), (0, 0, 20, 15))
+
+    def render(self, screen):
+        screen.blit(self.image, self.pos.as_point())
+        pygame.draw.line(screen, (0, 255, 0), self.pos.as_point(), (self.pos + self.speed*5).as_point())
+        self.draw()
+
+
+run = True
+pygame.init()
+pygame.display.set_mode(SIZE)
+screen = pygame.display.get_surface()
+
+ship = Ship((100, 100))
+clock = pygame.time.Clock()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                sys.exit()
+        ship.events(event)
+    clock.tick(FPS)
+    ship.update()
+    screen.fill((0, 0, 0))
+    ship.render(screen)
+    pygame.display.flip()
---
+++ Vectors.py
@@ -0,0 +1,34 @@
+import math
+
+
+class Vector:
+    def __init__(self, coordinates):
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+
+    def __str__(self):
+        return '(%.2f, %.2f)' % (self.x, self.y)
+
+    def __add__(self, other):
+        return Vector((self.x + other.x, self.y + other.y))
+
+    def __mul__(self, other):
+        return Vector((self.x * other, self.y * other))
+
+    def __sub__(self, other):
+        return self + other * -1
+
+    def get_len(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def rotate(self, angle):
+        x = self.x
+        y = self.y
+        self.x = x*math.cos(math.radians(angle)) - y*math.sin(math.radians(angle))
+        self.y = y*math.cos(math.radians(angle)) + x*math.sin(math.radians(angle))
+
+    def as_point(self):
+        return self.x, self.y
+
+    def normal(self):
+        return Vector((self.x/self.get_len(), self.y/self.get_len()))
