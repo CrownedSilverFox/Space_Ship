92ee457ea4b9ba85bf1be77ab50f89f36462327e
crownedsilverfox <byskillcfpro@gmail.com>
2015-12-12 19:04:57 +0700

    Хрень какая-то.

--- Main.py
+++ Main.py
@@ -7,24 +7,25 @@ TURN_LEFT = 1
 TURN_RIGHT = 2
 SLOWLY = 3
 FASTER = 4
-FPS = 40
+FPS = 400
 SIZE = (800, 600)
 
 
 class Ship:
+    #  Космический корабль-килька
     def __init__(self, pos):
         self.pos = Vector(pos)
         self.speed = Vector((1, 0))
         self.image = pygame.Surface((100, 100))
         self.status = NORMAL
         self.vect = self.speed
-        self.speed_up = 0.4
+        self.speed_up = 100
 
     def move(self):
         if self.status == TURN_LEFT:
-            self.speed.rotate(-10)
+            self.speed.rotate(-30)
         elif self.status == TURN_RIGHT:
-            self.speed.rotate(10)
+            self.speed.rotate(30)
         elif self.status == SLOWLY:
             if self.speed.get_len() == 0:
                 return
@@ -32,7 +33,7 @@ class Ship:
                 self.vect = self.speed.normal() * self.speed_up
                 self.speed = Vector((0, 0))
             else:
-                self.speed -= (self.speed.normal())
+                self.speed -= (self.speed.normal()) * self.speed_up
         elif self.status == FASTER:
             if self.speed.get_len() == 0:
                 self.speed = self.vect
@@ -67,11 +68,13 @@ class Ship:
         screen.blit(self.image, self.pos)
 
     def draw(self):
-        pygame.draw.rect(self.image, (100, 100, 255), (0, 0, 20, 15))
+        pygame.draw.rect(self.image, (200, 200, 255), (10, 10, 20, 10))
+        pygame.draw.polygon(self.image, (200, 200, 255), ((0, 15), (10, 10), (10, 20)))
+        pygame.draw.polygon(self.image, (200, 200, 255), ((15, 10), (18, 8), (18, 22)))
 
     def render(self, screen):
         screen.blit(self.image, self.pos.as_point())
-        pygame.draw.line(screen, (0, 255, 0), self.pos.as_point(), (self.pos + self.speed*5).as_point())
+        #pygame.draw.line(screen, (100, 255, 100), self.pos.as_point(), (self.pos + self.speed*5).as_point())
         self.draw()
 
 
@@ -92,6 +95,7 @@ while True:
         ship.events(event)
     clock.tick(FPS)
     ship.update()
+    print(ship.speed.get_len())
     screen.fill((0, 0, 0))
     ship.render(screen)
     pygame.display.flip()
